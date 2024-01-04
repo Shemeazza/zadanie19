dffdf58879c102da7c31acd8243d27142f8f2937
Shemeazza <ans@ans.local>
2024-01-04 17:55:10 +0100

    kula

--- Liczenie obietosci.c
+++ Liczenie obietosci.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 int a;
 int b;
 int c;
+float kula = 0.0;
+
+int obliczprosto(int a, int b, int c){
+    return(a*b*c);
+}
+float obliczkula(int a){
+	float b = (4/3.0)*M_PI*pow(a,3);
+	return b;
+}
 
 int main()
 {
-    printf("Podaj kolejne wartoœci:\n");
+    printf("Podaj Promien:\n");
     scanf("%d",&a);
-    scanf("%d",&b);
-    scanf("%d",&c);
-    printf("Wynik to: %d", oblicz(a,b,c));
+    kula = obliczkula(a);
+    printf("Wynik to: %f", kula);   
     return 0;
-}
-
-int oblicz(int a, int b, int c){
-    return(a*b*c);
 }
